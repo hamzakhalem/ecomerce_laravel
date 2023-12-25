3d9f8ca84e6bd02808dfe403608b94c3540f9683
hamza <hamzakhalem97@gmail.com>
2023-12-25 17:23:14 +0100

    admin contoller and login

--- resources/views/backend/layouts/auth-layout.blade.php
+++ resources/views/backend/layouts/auth-layout.blade.php
@@ -6,9 +6,9 @@
 	<title>@yield('title')</title>
 
 	<!-- Site favicon -->
-	<link rel="apple-touch-icon" sizes="180x180" href="backend/vendors/images/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="backend/vendors/images/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="backend/vendors/images/favicon-16x16.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/backend/vendors/images/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/backend/vendors/images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/backend/vendors/images/favicon-16x16.png">
 
 	<!-- Mobile Specific Metas -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -16,9 +16,9 @@
 	<!-- Google Font -->
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 	<!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="backend/vendors/styles/core.css">
-	<link rel="stylesheet" type="text/css" href="backend/vendors/styles/icon-font.min.css">
-	<link rel="stylesheet" type="text/css" href="backend/vendors/styles/style.css">
+	<link rel="stylesheet" type="text/css" href="/backend/vendors/styles/core.css">
+	<link rel="stylesheet" type="text/css" href="/backend/vendors/styles/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="/backend/vendors/styles/style.css">
 
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
@@ -31,7 +31,7 @@
 		<div class="container-fluid d-flex justify-content-between align-items-center">
 			<div class="brand-logo">
 				<a href="login.html">
-					<img src="backend/vendors/images/deskapp-logo.svg" alt="">
+					<img src="/backend/vendors/images/deskapp-logo.svg" alt="">
 				</a>
 			</div>
 			<div class="login-menu">
@@ -45,7 +45,7 @@
 		<div class="container">
 			<div class="row align-items-center">
 				<div class="col-md-6 col-lg-7">
-					<img src="backend/vendors/images/login-page-img.png" alt="">
+					<img src="/backend/vendors/images/login-page-img.png" alt="">
 				</div>
 				<div class="col-md-6 col-lg-5">
                     @yield('content')
@@ -54,10 +54,10 @@
 		</div>
 	</div>
 	<!-- js -->
-	<script src="backend/vendors/scripts/core.js"></script>
-	<script src="backend/vendors/scripts/script.min.js"></script>
-	<script src="backend/vendors/scripts/process.js"></script>
-	<script src="backend/vendors/scripts/layout-settings.js"></script>
+	<script src="/backend/vendors/scripts/core.js"></script>
+	<script src="/backend/vendors/scripts/script.min.js"></script>
+	<script src="/backend/vendors/scripts/process.js"></script>
+	<script src="/backend/vendors/scripts/layout-settings.js"></script>
     @stack('scripts')
 </body>
 </html>
--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin_test', function () {
-    return 'hello world';
+Route::prefix('admin')->group(function(){
+
+    Route::middleware(['guest:admin'])->group(function (){
+        Route::view('/login', 'backend.pages.admin.auth.login')->name('login');
+    });
+
+    Route::middleware(['auth:admin'])->group(function (){
+        Route::view('/home', 'backend.pages.admin.home')->name('login');
+    });
+
 });
