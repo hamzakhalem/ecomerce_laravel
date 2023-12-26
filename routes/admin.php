cf04935213eed9d51f57db421dcf2f603fb6ca3f
hamza <hamzakhalem97@gmail.com>
2023-12-26 18:00:09 +0100

    new middleware

--- routes/admin.php
+++ routes/admin.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 Route::prefix('admin')->group(function(){
 
-    Route::middleware(['guest:admin'])->group(function (){
+    Route::middleware(['guest:admin', 'PreventBackHistory'])->group(function (){
         Route::view('/login', 'backend.pages.admin.auth.login')->name('admin.login');
         Route::post('/loginhandler',[ AdminController::class, 'loginHandler'] )->name('login_handler');
     });
 
-    Route::middleware(['auth:admin'])->group(function (){
+    Route::middleware(['auth:admin', 'PreventBackHistory'])->group(function (){
         Route::view('/home', 'backend.pages.admin.home')->name('admin.home');
         Route::get('/logouthandler',[ AdminController::class, 'logoutHandler'] )->name('admin.logout');
 
